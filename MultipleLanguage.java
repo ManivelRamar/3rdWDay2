b082ef3e1045c746f733ba123c5cc85dc92a3fbd
Manivel <96624633+ManivelRamar@users.noreply.github.com>
2022-01-15 00:50:58 +0530

    Add files via upload

---
+++ MultipleLanguage.java
@@ -0,0 +1,14 @@
+package week3.day2;
+
+public abstract class MultipleLanguage implements TestTool
+{
+	public void Python() 
+	{
+		System.out.println("Python is a good language for automation");
+	}
+	
+	public void Ruby() 
+	{
+		
+	}
+}
